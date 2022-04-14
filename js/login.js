682d6843b56d3e40d603ed316afadf4f5ad0d47f
javierbull <javierbullreyes@gmail.com>
2022-04-14 14:26:18 -0500

    nase

---
+++ js/login.js
@@ -0,0 +1,40 @@
+"use strict";
+
+$(document).ready(function(){
+
+  // SHOW-HIDE PASSWORD
+  $(".toggle-password").click(function() {
+
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+// END SHOW-HIDE PASSWORD
+
+// Select2
+
+// $(".select2").select2({
+//   theme: 'bootstrap4',
+// });
+//
+// $(".select2-multiple").select2({
+//   theme: 'bootstrap4',
+// });
+
+// End select2
+
+$("#show-resumen").click(function() {
+  $("#content-sorteo").hide();
+  $("#content-resumen").show();
+});
+
+$("#show-sorteo").click(function() {
+  $("#content-sorteo").show();
+  $("#content-resumen").hide();
+});
+
+});
