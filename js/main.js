7d7314670f686ebf5412d3f996b730b533c71e6d
javierbull <javierbullreyes@gmail.com>
2022-04-15 17:51:29 -0500

    btn disebar toggle

--- js/main.js
+++ js/main.js
@@ -45,3 +45,14 @@ $(".sidebar-admin button").click(function(){
 // End admin dashboard
 
 });
+
+// admin btn sidebar togle menu
+const btnToggle = document.querySelector('#toggle-btn-sidebar');
+
+btnToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('sidebar-admin').classList.toggle('active');
+  console.log(document.getElementById('sidebar-admin'))
+});
+
+// End admin btn sidebar togle menu
