b39b5ad759764d0382f3bbc908f5725884b0a945
javierbull <javierbullreyes@gmail.com>
2023-01-12 18:28:32 -0500

    select 2 multiple base

--- js/main.js
+++ js/main.js
@@ -336,6 +336,32 @@ $('#comprobanteSelectorSe').change(function(){
 });
 // END SHOW HIDE SELECT
 
+// ACTIVAR SELECT 2
+$('.select2').select2({
+  theme: "bootstrap-5",
+  width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+  placeholder: $(this).data('placeholder')
+});
+
+$('.select2modal').each(function() {
+  $(this).select2({
+    theme: "bootstrap-5",
+    width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style',
+    placeholder: $(this).data('placeholder'),
+    dropdownParent: $(this).parent()
+  });
+});
+
+$( '.select2-multiple' ).select2( {
+    theme: "bootstrap-5",
+    width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+    placeholder: $( this ).data( 'placeholder' ),
+    closeOnSelect: false,
+    allowClear: true,
+} );
+
+// END ACTIVAR SELECT2
+
 
 });
 
